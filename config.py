ec670e924fd7049f4ed83c3022bd0bcaf1aef181
andtoro <andrea.torosantucci.at@gmail.com>
2025-10-22 11:02:09 +0200

    adjusted files paths

---
+++ config.py
@@ -0,0 +1,16 @@
+# Configuration file with all settings
+import os
+
+# Base directories
+BASE_DIR = os.path.dirname(__file__)             # directory of config.py
+NETCDF_FILE = os.path.join(BASE_DIR, "data")
+
+REFLECTANCE_VAR = 'reflectance'
+TIME_DIM = 'time'
+LAT_DIM = 'lat'
+LON_DIM = 'lon'
+WAVELENGTH_DIM = 'wavelength'
+
+OUTPUT_DIR = 'outputs/'
+FIGURE_DIR = 'outputs/figures/'
+CSV_DIR = os.path.join(BASE_DIR, "outputs/csv")
