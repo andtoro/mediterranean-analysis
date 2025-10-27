1d099e083d50973b8eb1d89214fb4bbd8d0e40cb
andtoro <andrea.torosantucci.at@gmail.com>
2025-10-27 17:33:21 +0100

    added some paths and deleted others

--- config.py
+++ config.py
@@ -5,12 +5,8 @@ import os
 BASE_DIR = os.path.dirname(__file__)             # directory of config.py
 NETCDF_FILE = os.path.join(BASE_DIR, "data")
 
-REFLECTANCE_VAR = 'reflectance'
-TIME_DIM = 'time'
-LAT_DIM = 'lat'
-LON_DIM = 'lon'
 WAVELENGTH_DIM = ['412', '443', '490', '510', '555', '670']
 
 OUTPUT_DIR = 'outputs/'
-FIGURE_DIR = 'outputs/figures/'
+FIGURE_DIR = os.path.join(BASE_DIR, "outputs/figures")
 CSV_DIR = os.path.join(BASE_DIR, "outputs/csv")
